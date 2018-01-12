98e45931adf76b2a3e9f4b794e7ee15e8cfbcf6c
Deep1992 <35268638+Deep1992@users.noreply.github.com>
2018-01-12 11:00:52 -0500

    Add files via upload

---
+++ day5.sql
@@ -0,0 +1,317 @@
+-- MySQL dump 10.16  Distrib 10.1.21-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: localhost
+-- ------------------------------------------------------
+-- Server version	10.1.21-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `c`
+--
+
+DROP TABLE IF EXISTS `c`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `c` (
+  `customerid` int(2) DEFAULT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `nickname` varchar(10) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `postalcode` int(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `c`
+--
+
+LOCK TABLES `c` WRITE;
+/*!40000 ALTER TABLE `c` DISABLE KEYS */;
+/*!40000 ALTER TABLE `c` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `c1`
+--
+
+DROP TABLE IF EXISTS `c1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `c1` (
+  `customerid` int(2) DEFAULT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `nickname` varchar(10) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `postalcode` int(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `c1`
+--
+
+LOCK TABLES `c1` WRITE;
+/*!40000 ALTER TABLE `c1` DISABLE KEYS */;
+/*!40000 ALTER TABLE `c1` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `c2`
+--
+
+DROP TABLE IF EXISTS `c2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `c2` (
+  `ID` int(3) NOT NULL,
+  `LasteName` varchar(255) NOT NULL,
+  `FirstName` varchar(255) DEFAULT NULL,
+  `Age` int(3) DEFAULT NULL,
+  `CityID` int(4) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fk_persons` (`CityID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `c2`
+--
+
+LOCK TABLES `c2` WRITE;
+/*!40000 ALTER TABLE `c2` DISABLE KEYS */;
+/*!40000 ALTER TABLE `c2` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cities`
+--
+
+DROP TABLE IF EXISTS `cities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cities` (
+  `customerid` int(2) NOT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `nickname` varchar(10) DEFAULT NULL,
+  `address` varchar(40) NOT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `postalcode` int(6) DEFAULT NULL,
+  PRIMARY KEY (`customerid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cities`
+--
+
+LOCK TABLES `cities` WRITE;
+/*!40000 ALTER TABLE `cities` DISABLE KEYS */;
+INSERT INTO `cities` VALUES (1,'gagan','deep','west','toronto',123456),(2,'kiran','deep','east','toronto',479456),(3,'amandeep','aman','banting','brampton',847239),(4,'simran','simu','tomken','missisauga',367298);
+/*!40000 ALTER TABLE `cities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `citymaster`
+--
+
+DROP TABLE IF EXISTS `citymaster`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `citymaster` (
+  `CityID` int(4) NOT NULL,
+  `cityName` varchar(255) DEFAULT 'toronto',
+  PRIMARY KEY (`CityID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `citymaster`
+--
+
+LOCK TABLES `citymaster` WRITE;
+/*!40000 ALTER TABLE `citymaster` DISABLE KEYS */;
+/*!40000 ALTER TABLE `citymaster` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cust`
+--
+
+DROP TABLE IF EXISTS `cust`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cust` (
+  `customerid` int(2) DEFAULT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `nickname` varchar(10) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `postalcode` int(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cust`
+--
+
+LOCK TABLES `cust` WRITE;
+/*!40000 ALTER TABLE `cust` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cust` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cust1`
+--
+
+DROP TABLE IF EXISTS `cust1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cust1` (
+  `customerid` int(2) DEFAULT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `nickname` varchar(10) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `postalcode` int(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cust1`
+--
+
+LOCK TABLES `cust1` WRITE;
+/*!40000 ALTER TABLE `cust1` DISABLE KEYS */;
+INSERT INTO `cust1` VALUES (1,'gagan','deep','west','toronto',123456),(2,'kiran','deep','east','toronto',479456),(3,'amandeep','aman','banting','brampton',847239),(4,'simran','simu','tomken','missisauga',367298);
+/*!40000 ALTER TABLE `cust1` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cust2`
+--
+
+DROP TABLE IF EXISTS `cust2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cust2` (
+  `customerid` int(2) DEFAULT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `nickname` varchar(10) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `postalcode` int(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cust2`
+--
+
+LOCK TABLES `cust2` WRITE;
+/*!40000 ALTER TABLE `cust2` DISABLE KEYS */;
+INSERT INTO `cust2` VALUES (1,'gagan','deep','west','toronto',123456),(2,'kiran','deep','east','toronto',479456),(3,'amandeep','aman','banting','brampton',847239);
+/*!40000 ALTER TABLE `cust2` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `ID` int(3) NOT NULL,
+  `LasteName` varchar(255) NOT NULL,
+  `FirstName` varchar(255) DEFAULT NULL,
+  `Age` int(3) DEFAULT NULL,
+  `CityID` int(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customers` (
+  `customerid` int(2) DEFAULT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `nickname` varchar(10) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `postalcode` int(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (1,'gagan','deep','west','toronto',123456),(2,'kiran','deep','east','toronto',479456),(3,'amandeep','aman','banting','brampton',847239),(4,'simran','simu','tomken','missisauga',367298);
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `persons`
+--
+
+DROP TABLE IF EXISTS `persons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `persons` (
+  `ID` int(3) NOT NULL,
+  `LasteName` varchar(255) NOT NULL,
+  `FirstName` varchar(255) DEFAULT NULL,
+  `Age` int(3) DEFAULT NULL,
+  `CityID` int(4) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fk_persons` (`CityID`),
+  CONSTRAINT `fk_persons` FOREIGN KEY (`CityID`) REFERENCES `citymaster` (`CityID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `persons`
+--
+
+LOCK TABLES `persons` WRITE;
+/*!40000 ALTER TABLE `persons` DISABLE KEYS */;
+/*!40000 ALTER TABLE `persons` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-01-12 10:57:24
